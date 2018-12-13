efa7f8767c0a171327dc48f5c09b7821b69ceee8
Tadeo Kondrak <me@tadeo.ca>
2018-12-13 16:53:46 -0700

    add secret param and remove enabledbydefault

--- modules/echo.js
+++ modules/echo.js
@@ -1,11 +1,11 @@
 module.exports = {
   name: 'echo',
-  enabledByDefault: true,
   commands: [
     {
       name: 'echo',
       description: 'Takes your input and spits it right back at you.',
       examples: ['echo yes'],
+      secret: false,
       permissionLevel: 0, // 0: anyone, 1: server admins, 2: bot admins
       aliases: [],
       cooldown: 0, // per user not server
