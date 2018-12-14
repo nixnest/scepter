27dec56ef42c6798d0264f7731fe77b44d2ec2e5
Tadeo Kondrak <me@tadeo.ca>
2018-12-14 11:45:32 -0700

    change echo to mod command

--- modules/echo.js
+++ modules/echo.js
@@ -6,7 +6,7 @@ module.exports = {
       secret: false, // optional, default false unless permissionLevel is 3, then it's true
       description: 'Takes your input and spits it right back at you.', // required if not secret
       examples: ['yes'], // required if not secret
-      permissionLevel: 0, // optional, default 0 | 0: anyone, 1: server mods, 2: server admins, 3: bot admins
+      permissionLevel: 1, // optional, default 0 | 0: anyone, 1: server mods, 2: server admins, 3: bot admins
       aliases: [], // optional
       cooldown: 0, // optional, default 0
       minArgs: 1, // required
