169db27941c464b0ce3ebc627db3fd9f9b21277e
Tadeo Kondrak <me@tadeo.ca>
2018-12-13 17:23:59 -0700

    remove redundant 'echo' from example

--- modules/echo.js
+++ modules/echo.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       name: 'echo',
       description: 'Takes your input and spits it right back at you.',
-      examples: ['echo yes'],
+      examples: ['yes'],
       secret: false,
       permissionLevel: 0, // 0: anyone, 1: server admins, 2: bot admins
       aliases: [],
