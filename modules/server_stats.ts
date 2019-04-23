0e2a55253c5aa1b26668df7a907b190dfb91c323
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2019-04-22 22:20:01 -0300

    Stats categories (#21)
    * feat(stats): Implements channel category filtering
    
    * fix(stats): Fix styles

--- modules/server_stats.ts
+++ modules/server_stats.ts
@@ -1,41 +1,52 @@
-import { Message, Client, RichEmbed, MessageEmbedField } from 'discord.js'
+import { Message, Client, RichEmbed } from 'discord.js'
+import { allChannelsByCategory } from '../lib/channels'
 
-let buildEmbed = async (data) => {
-  let embed = new RichEmbed()
-  let embedField: [string, {}][] | any[][]
-  let embedValue: MessageEmbedField
+const _buildEmbed = (embed: RichEmbed, data) => {
+  const _buildRoles = (data) => {
+    const key = 'roles'
+    embed.addField(key, data.fields[key].map(x => `${x[1]} (${x[0]}): ${x[2]} members`))
+  }
+
+  const _buildChannels = (data) => {
+    const key = 'channels:'
+    embed.addField(key, '--------')
+    const channelsData = Object.entries(data.fields.channels)
+    channelsData.map(x => {
+      embed.addField(`${x[0]}:`, (x[1] as []).map(y => `${y[1]} (${y[0]}): ${y[2]}`))
+      // TODO: we have a types conflict here. Define a stats Type
+    })
+  }
+
   embed.setTitle(`Server stats for ${data.name}`)
-  data.fields.map(field => {
-    embedField = Object.entries(field)
-    embedValue = embedField[0][1].map(val => Object.values(val))
-                                 .map(x => `${x[1]} (${x[0]})`)
-    embed.addField(embedField[0][0], embedValue)
-  })
+  _buildRoles(data)
+  _buildChannels(data)
   embed.setFooter(`Server created at ${data.creationDate}`)
   return embed
 }
 
-let refreshCache = async (client: Client): Promise<void> => {
+const buildEmbed = (data) => {
+  // TODO: Export as generic function and define _buildEmbed requirements?
+  let embed = new RichEmbed()
+  return _buildEmbed(embed, data)
+}
+
+const refreshCache = async (client: Client): Promise<void> => {
   for (let guild of client.guilds) {
     const clientGuild = guild[1]
     const guildData = {
       name: clientGuild.name,
       creationDate: clientGuild.createdAt,
-      fields: [
-        {
-          roles: clientGuild.roles.map(x => [x.id, x.name])
-        },
-        {
-          channels: clientGuild.channels.map(x => [x.id, x.name])
-        }
-      ]
+      fields: {
+        roles: clientGuild.roles.map(x => [x.id, x.name, x.members.size]),
+        channels: allChannelsByCategory(clientGuild.channels.array())
+      }
     }
     await client['guildData'].set(`${clientGuild.id}.stats`, guildData)
   }
 }
 
-let stats = async (message: Message, _) => {
-  const embedMessage = await buildEmbed(message.client['guildData'].get(`${message.guild.id}.stats`))
+const stats = async (message: Message, _) => {
+  const embedMessage = buildEmbed(message.client['guildData'].get(`${message.guild.id}.stats`))
   return message.channel.send(embedMessage)
 }
 
