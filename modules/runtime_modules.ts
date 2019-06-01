bdef9d4d884912fdb6a3a0b734e7ca929d2d3255
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2019-06-01 01:05:03 -0300

    feat: Adds command to access module unload feature

--- modules/runtime_modules.ts
+++ modules/runtime_modules.ts
@@ -1,8 +1,12 @@
 import { Message } from 'discord.js'
-import { loadModule as loadKernelModule } from '../scepter'
+import { loadModule, unloadModule } from '../scepter'
 
-const loadModule = async (message: Message, args: string[]) => {
-  args.forEach(loadKernelModule)
+const loadModules = async (message: Message, args: string[]) => {
+  args.forEach(loadModule)
+}
+
+const unloadModules = async (message: Message, args: string[]) => {
+  args.forEach(unloadModule)
 }
 
 export const name = 'runtime modules'
@@ -12,6 +16,13 @@ export const commands = [
     description: 'Attempts to load an available module',
     examples: ['loadmod echo'],
     minArgs: 1,
-    run: loadModule
+    run: loadModules
+  },
+  {
+    name: 'unloadmod',
+    description: 'Attempts to deactivate a loaded module',
+    exampled: ['unloadmod echo'],
+    minArgs: 1,
+    run: unloadModules
   }
 ]
