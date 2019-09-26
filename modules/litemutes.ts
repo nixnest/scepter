cfbf97334ee15448369209c18498138d19ef82e5
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2019-09-26 14:41:00 -0700

    Update litemutes.ts
    fix lint

--- modules/litemutes.ts
+++ modules/litemutes.ts
@@ -2,21 +2,21 @@ import { Client, Guild, GuildMember, Role, Message, User } from 'discord.js'
 
 import * as log from '../lib/log'
 
-const INDEFINITE_LiteMute = Math.pow(2, 64)
+const INDEFINITE_LITEMUTE = Math.pow(2, 64)
 
 const _getGuildLiteMutesRole = async (guild: Guild): Promise<Role> => {
   let role = guild.client['guildData'].get(`${guild.id}.LiteMutedRole`)
   if (role == null) {
-    role = guild.roles.find(x => x.name === 'LiteMuted')
+    role = guild.roles.find(x => x.name === 'lite-muted')
     if (role == null) {
-      throw new Error('No LiteMutes role set and a default role could not be found.')
+      throw new Error('No lite-muted role set and a default role could not be found.')
     }
   }
   return role
 }
 
-const _removeMemberFromLiteMutes = async (client: Client, LiteMuteId: string) => {
-  await client['timerData'].delete(LiteMuteId)
+const _removeMemberFromLiteMutes = async (client: Client, liteMuteId: string) => {
+  await client['timerData'].delete(liteMuteId)
 }
 
 const checkLiteMutes = async (client: Client) => {
@@ -25,12 +25,12 @@ const checkLiteMutes = async (client: Client) => {
   let guildMember: GuildMember
   let clientGuildId: string
   let guildMemberId: string
-  let LiteMuteRole: Role
+  let liteMuteRole: Role
 
   console.log(client['timerData'].entries())
 
   for (let entry of client['timerData'].entries()) {
-    if (entry[1] === INDEFINITE_LiteMute) continue
+    if (entry[1] === INDEFINITE_LITEMUTE) continue
     if (curDate > entry[1]) {
       // UnLiteMute the user
       [clientGuildId, guildMemberId] = entry[0].split('.')
@@ -44,14 +44,14 @@ const checkLiteMutes = async (client: Client) => {
       }
 
       try {
-        LiteMuteRole = await _getGuildLiteMutesRole(clientGuild)
+        liteMuteRole = await _getGuildLiteMutesRole(clientGuild)
       } catch (e) {
         log.warn(e)
         continue
       }
 
       try {
-        await guildMember.removeRole(LiteMuteRole)
+        await guildMember.removeRole(liteMuteRole)
       } catch (e) {
         log.warn(`${name}.job: ${e}`)
         return
@@ -67,54 +67,54 @@ const setLiteMutedRole = async (message: Message, args: string[]) => {
   const roleName = message.guild.roles.get(roleId)
   let returnMessage: String
   if (roleName) {
-    await message.client['guildData'].set(`${message.guild.id}.LiteMutedRole`, roleId)
-    returnMessage = `Role ${roleName} has been set as the LiteMuted role.`
+    await message.client['guildData'].set(`${message.guild.id}.liteMutedRole`, roleId)
+    returnMessage = `Role ${roleName} has been set as the lite-muted role.`
   } else {
     returnMessage = `That role does not exist, dummy.`
   }
   return message.channel.send(returnMessage)
 }
 
-const LiteMute = async (message: Message, args: string[]) => {
-  const LiteMuteTargetId = message.mentions.users.first().id
+const liteMute = async (message: Message, args: string[]) => {
+  const liteMuteTargetId = message.mentions.users.first().id
   const curDate = new Date()
-  const LiteMutePeriod = args[1]
+  const liteMutePeriod = args[1]
     ? new Date(curDate.setSeconds(curDate.getSeconds() + parseInt(args[1], 10)))
-    : INDEFINITE_LiteMute
-  const LiteMuteRole: Role = await _getGuildLiteMutesRole(message.guild)
-  const LiteMuteTarget: GuildMember = await message.guild.fetchMember(LiteMuteTargetId)
+    : INDEFINITE_LITEMUTE
+  const liteMuteRole: Role = await _getGuildLiteMutesRole(message.guild)
+  const liteMuteTarget: GuildMember = await message.guild.fetchMember(liteMuteTargetId)
 
   try {
-    await LiteMuteTarget.addRole(LiteMuteRole)
+    await liteMuteTarget.addRole(liteMuteRole)
   } catch {
     return message.channel.send("I don't have permission to manage roles!")
   }
-  await message.client['timerData'].set(`${message.guild.id}.${LiteMuteTargetId}`, LiteMutePeriod)
+  await message.client['timerData'].set(`${message.guild.id}.${liteMuteTargetId}`, liteMutePeriod)
 
-  const LiteMutePeriodText = LiteMutePeriod === INDEFINITE_LiteMute ? 'indefinitely' : `for ${args[1]} seconds`
-  return message.channel.send(`User ${args[0]} has been LiteMuted ${LiteMutePeriodText}.`)
+  const liteMutePeriodText = liteMutePeriod === INDEFINITE_LITEMUTE ? 'indefinitely' : `for ${args[1]} seconds`
+  return message.channel.send(`User ${args[0]} has been lite-muted ${liteMutePeriodText}.`)
 }
 
 const unLiteMute = async (message: Message, args: string[]) => {
-  const LiteMuteTargetId = message.mentions.users.first().id
-  const LiteMuteRole = await _getGuildLiteMutesRole(message.guild)
-  const LiteMuteTarget = await message.guild.fetchMember(LiteMuteTargetId)
+  const liteMuteTargetId = message.mentions.users.first().id
+  const liteMuteRole = await _getGuildLiteMutesRole(message.guild)
+  const liteMuteTarget = await message.guild.fetchMember(liteMuteTargetId)
 
   try {
-    await LiteMuteTarget.removeRole(LiteMuteRole)
+    await liteMuteTarget.removeRole(liteMuteRole)
   } catch {
     return message.channel.send("I don't have permission to manage roles!")
   }
 
-  message.client['timerData'].delete(`${message.guild.id}.${LiteMuteTargetId}`)
-  return message.channel.send(`User ${args[0]} has been unLiteMuted.`)
+  message.client['timerData'].delete(`${message.guild.id}.${liteMuteTargetId}`)
+  return message.channel.send(`User ${args[0]} has been un-lite-muted.`)
 }
 
 const newcomerLiteMuteCheck = async (member: GuildMember) => {
   const LiteMuteRole = await _getGuildLiteMutesRole(member.guild)
   if (member.client['timerData'].has(`${member.guild.id}.${member.id}`)) {
     try {
-      await member.addRole(LiteMuteRole)
+      await member.addRole(liteMuteRole)
     } catch (e) {
       log.warn(`${name}.newcomerLiteMuteCheck: ${e}`)
       return
@@ -123,21 +123,21 @@ const newcomerLiteMuteCheck = async (member: GuildMember) => {
 }
 
 const processManualLiteMute = async (previous: GuildMember, actual: GuildMember) => {
-  let LiteMuteRole: Role | null
+  let liteMuteRole: Role | null
 
   try {
-    LiteMuteRole = await _getGuildLiteMutesRole(actual.guild)
+    liteMuteRole = await _getGuildLiteMutesRole(actual.guild)
   } catch (e) {
     log.warn(e)
     return
   }
 
   const isListed = actual.client['timerData'].has(`${actual.guild.id}.${actual.id}`)
-  const hadRole = previous.roles.has(LiteMuteRole.toString())
-  const hasRole = actual.roles.has(LiteMuteRole.toString())
+  const hadRole = previous.roles.has(liteMuteRole.toString())
+  const hasRole = actual.roles.has(liteMuteRole.toString())
 
   if (!isListed && !hadRole && hasRole) {
-    await actual.client['timerData'].set(`${actual.guild.id}.${actual.id}`, INDEFINITE_LiteMute)
+    await actual.client['timerData'].set(`${actual.guild.id}.${actual.id}`, INDEFINITE_LITEMUTE)
   }
   if (isListed && hadRole && !hasRole) {
     await actual.client['timerData'].delete(`${actual.guild.id}.${actual.id}`)
@@ -148,12 +148,12 @@ const removeLiteMuteFromBannedUser = async (guild: Guild, user: User) => {
   return _removeMemberFromLiteMutes(guild.client, `${guild.id}.${user.id}`)
 }
 
-export const name = 'LiteMute'
+export const name = 'lite-mute'
 export const commands = [
   {
     name: 'setlitemutedrole',
     description: 'Sets the given role ID to use as the LiteMute role',
-    examples: ['setLiteMutedrole 522963469046120479'],
+    examples: ['setlitemutedrole 522963469046120479'],
     secret: false,
     permissionLevel: 2,
     minArgs: 1,
@@ -170,12 +170,12 @@ export const commands = [
     minArgs: 1,
     maxArgs: 2,
     aliases: [],
-    run: LiteMute
+    run: liteMute
   },
   {
     name: 'unlitemute',
-    description: 'Manually unLiteMutes an user',
-    examples: ['unLiteMute @Sky Aviatour#3415'],
+    description: 'Manually un-litemutes an user',
+    examples: ['unlitemute @Sky Aviatour#3415'],
     secret: false,
     permissionLevel: 1,
     minArgs: 1,
