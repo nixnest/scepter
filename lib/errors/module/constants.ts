f6c1210bfd85f84a0d6f67cd6f25bfb821ce0f83
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2019-10-05 14:21:51 -0300

    chore(): reworks errors structure for core lib

---
+++ lib/errors/module/constants.ts
@@ -0,0 +1,9 @@
+export const MODULE_ERROR = 'ModuleError'
+export const MODULE_LOAD_ERROR = 'ModuleLoadError'
+export const MODULE_UNLOAD_ERROR = 'ModuleUnloadError'
+
+export const MODULE_ERRORS = [
+  MODULE_ERROR,
+  MODULE_LOAD_ERROR,
+  MODULE_UNLOAD_ERROR
+]
